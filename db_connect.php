3278361aa6cc211055f50eb2099151d597134154
范志康 <goodman.capu@gmail.com>
2016-04-15 13:25:10 +0800

    MySQL connection test

--- db_connect.php
+++ db_connect.php
@@ -4,11 +4,13 @@
  */
 require_once 'config.php';
 
-// Database connector.
-function dbconnect() {
-    $con = @mysql_connect(SOCIAL_DB_HOSTNAME, SOCIAL_DB_USERNAME, SOCIAL_DB_PASSWORD) or die("Cannot connect to database!");
-    mysql_query("SET NAMES 'UTF8'");
-    mysql_select_db(SOCIAL_DB_NAME);
+/**
+ * Database connector.
+ */
+function db_connect() {
+    $con = mysqli_connect(SOCIAL_DB_HOSTNAME, SOCIAL_DB_USERNAME, SOCIAL_DB_PASSWORD, SOCIAL_DB_NAME) or die("数据库不存在或用户名密码不正确");
+    $con->query("SET NAMES 'UTF8'");
+    return $con;
 }
 
 ?>
--- initialize.sql
+++ initialize.sql
@@ -3,9 +3,12 @@
 -- Table structure for table `user`
 
 CREATE TABLE `user` (
-	`id` varchar(128) NOT NULL,
-	`name` varchar(30) NOT NULL,
-	`register_time` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
-	`latest_time` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
-	PRIMARY KEY(`id`)
+  `userid` VARCHAR(16) NOT NULL,
+  `name` VARCHAR(16) NOT NULL,
+  `sex` VARCHAR(16) NOT NULL,
+  `email` VARCHAR(128) NOT NULL,
+  `icon` VARCHAR(128) NOT NULL,
+  `register_time` DATETIME NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `latest_time` DATETIME NOT NULL DEFAULT '1000-01-01 00:00:00',
+  PRIMARY KEY (`userid`)
 );
