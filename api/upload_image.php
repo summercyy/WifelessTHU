6778661e3b45f3be09ad598247e3d33e13736578
范志康 <goodman.capu@gmail.com>
2016-04-27 18:17:21 +0800

    Upload Image API

---
+++ api/upload_image.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Frank
+ * Date: 16/4/27
+ * Time: 下午5:48
+ */
+
+/**
+ * Common functions for Social API.
+ */
+require_once 'api_utilities.php';
+$con = db_connect();
+check_login($con);
+
+$image = filter($con, $_POST["image"]);
+$type = filter($con, $_POST["type"]);
+
+$max_length = 0;
+switch ($type) {
+    case "icon":
+        $max_length = 50;
+        break;
+    case "image":
+        $max_length = 200;
+        break;
+    default:
+        report_error(ERROR_ILLEGAL_PARAMETER, "图片类型不合法");
+        break;
+}
+save_image($image, $type, $max_length);
