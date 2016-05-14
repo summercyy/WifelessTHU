0da706b528f1f4067bfe2c6877b3fe8c204dbd91
archimekai <wenkai123111@126.com>
2016-05-15 00:50:23 +0800

    send_post_api_start

---
+++ api/index.php
@@ -0,0 +1,19 @@
+<?PHP
+// 最好能在文件头中也返回错误代码
+header('HTTP/1.1 403 Forbidden');
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<title>403 Forbidden</title>
+</head>
+
+<body>
+
+<h1 align="center">Directory access is forbidden.</h1>
+
+</body>
+
+</html>
