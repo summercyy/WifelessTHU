16b12785cb872fbc757326d1b149f90a6fbd7151
范志康 <goodman.capu@gmail.com>
2016-06-25 21:47:49 +0800

    推荐好友SQL更改

--- api/recommend_friends.php
+++ api/recommend_friends.php
@@ -8,7 +8,12 @@ check_login($con);
 
 $userid = intval(filter($con, $_POST["userid"]));
 
-$result = $con->query("SELECT DISTINCT user.*, COUNT(*) AS num FROM friends AS fri1, friends AS fri2, user WHERE (fri1.followed_userid = $userid AND fri1.fan_userid = fri2.fan_userid) OR (fri1.fan_userid = $userid AND fri1.followed_userid = fri2.fan_userid) AND fri2.followed_userid = user.userid AND user.userid NOT IN ($userid) GROUP BY user.userid ORDER BY num DESC");
+$result = $con->query("SELECT DISTINCT user.*, COUNT(*) AS num FROM friends AS fri1, friends AS fri2, user 
+WHERE (fri1.followed_userid = $userid AND fri1.fan_userid = fri2.fan_userid)
+OR (fri1.fan_userid = $userid AND fri1.followed_userid = fri2.fan_userid)
+AND fri2.followed_userid = user.userid AND user.userid NOT IN ($userid)
+AND user.userid NOT IN (SELECT followed_userid FROM friends WHERE fan_userid = $userid)
+GROUP BY user.userid ORDER BY num DESC");
 
 check_sql_error($con);
 
