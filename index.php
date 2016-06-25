ddbf200c888c1428737f01f8fce079e966771272
范志康 <goodman.capu@gmail.com>
2016-06-25 17:42:20 +0800

    首页

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
-<!-- by Archimekai   首页，用于显示登陆和注册页面-->
-<!DOCTYPE html>
-<html>
-
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-	<title>欢迎来到 Wifeless THU</title>
-    <link rel="stylesheet" href="css/material_icon.css">
-    <link rel="stylesheet" href="css/material.indigo-pink.min.css">
-    <link rel="script" href="js/material.min.js">
+
+<?php
+
+$token = $_COOKIE["token"];
+
+if (strlen($token) == 0) {
+    echo "<meta http-equiv='refresh' content='0; url=login.php'>";
+} else {
+    echo "<meta http-equiv='refresh' content='0; url=homepage.php'>";
+}
+
+?>
+
 </head>
-
-<body>
-
-<h1 align="center">Welcome to Wifeless THU</h1>
-
-</body>
-
-</html>
